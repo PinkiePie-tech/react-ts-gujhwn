aa255a2568b8522badb5a9b611c82169ad1bb875
PinkiePie-tech <62440844+PinkiePie-tech@users.noreply.github.com>
2022-10-10 10:32:28 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Collection } from './Collection';
+import './style.css';
+
+export default function App() {
+  return <Collection />;
+}
---
+++ Collection.tsx
@@ -0,0 +1,12 @@
+import React = require('react');
+
+export class Collection extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    );
+  }
+}
